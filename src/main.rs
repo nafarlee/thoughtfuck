79aaf2fbf034421020e56dfc5e0a49ae3bf7b885
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-06 21:31:06 -0400

    Simplify reader loop

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ fn main() {
         let mut line = String::new();
         match std::io::stdin().read_line(&mut line) {
             Ok(0) => break,
-            Ok(_) => {
-                print!("{}", line);
-            },
+            Ok(_) => print!("{}", line),
             Err(error) => print!("{}", error),
         }
     }
