aaa9f85c20e0e390bd727c6490db8ca900208dd7
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-06 21:30:14 -0400

    Add input reader loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+        match std::io::stdin().read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => {
+                print!("{}", line);
+            },
+            Err(error) => print!("{}", error),
+        }
+    }
 }
