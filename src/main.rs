e61208a2b16292894bee6ce8e1536ae7307c65c0
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-06 21:52:56 -0400

    Add repl prompt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::io::Write;
+
 fn main() {
     loop {
+        print!("{}", "bf> ");
+        std::io::stdout().flush().unwrap();
+
         let mut line = String::new();
         match std::io::stdin().read_line(&mut line) {
             Ok(0) => break,
