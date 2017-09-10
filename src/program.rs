c8ed582a8bc34293f6bbc906f967df91118f6bb6
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-10 15:27:29 -0400

    Invoke handle_jump_forward if a '[' is seen

--- src/program.rs
+++ src/program.rs
@@ -35,10 +35,14 @@ impl Program {
             Some(mut index) => {
                 while index < self.instructions.len() {
                     let command = self.instructions[index];
-                    if command == Command::JumpForward { self.current_depth = self.current_depth + 1}
-                    if command == Command::JumpBackward { self.current_depth = self.current_depth - 1}
-                    vm.apply(command);
-                    index = index + 1;
+                    if command == Command::JumpForward {
+                        index = self.handle_jump_forward(vm, index);
+                    } else if command == Command::JumpBackward {
+                        self.current_depth = self.current_depth - 1
+                    } else {
+                        vm.apply(command);
+                        index = index + 1;
+                    }
                 }
                 self.instruction_pointer = Some(index);
             }
