e96da70a68e194677c83e8977756ef2a76bc2158
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-20 23:45:47 -0400

    Add repl function to contain repl specific runtime

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use program::Program;
 use std::io::Write;
 use std::env;
 
+
 fn main() {
     for argument in env::args() {
         println!("{}", argument);
@@ -15,6 +16,23 @@ fn main() {
 
     let mut vm = VM::new();
     let mut program = Program::new();
+    loop {
+        print!("{}", if program.is_seeking {"... "} else {"bf> "});
+        std::io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        let commands = match std::io::stdin().read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => parse::parse(&line),
+            Err(error) => panic!(error),
+        };
+        program.append(&commands);
+        program.execute(&mut vm);
+    }
+}
+
+
+fn repl (mut vm: VM, mut program: Program) -> () {
     loop {
         print!("{}", if program.is_seeking {"... "} else {"bf> "});
         std::io::stdout().flush().unwrap();
