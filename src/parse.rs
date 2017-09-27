1f7a62287155c983a3dc8e8769fc453ddbb49883
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-27 19:30:45 -0400

    Add unit test for parse::parse(&str)

--- src/parse.rs
+++ src/parse.rs
@@ -21,3 +21,24 @@ pub fn parse(s: &str) -> Vec<Command> {
     }
     vec
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse() {
+        const COMMAND_TOUR: &'static str = ".,><+-[]9";
+        let actual = parse(&COMMAND_TOUR);
+        let expected = [
+            Command::Output,
+            Command::Input,
+            Command::RightShift,
+            Command::LeftShift,
+            Command::Increment,
+            Command::Decrement,
+            Command::JumpForward,
+            Command::JumpBackward,
+        ];
+        assert_eq!(actual, expected);
+    }
+}
