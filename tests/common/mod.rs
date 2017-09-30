17fe809d2e656d36ea6a923a633f02ff7a21bef8
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-29 21:35:24 -0500

    Move common code for integration tests to common module

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,8 +1,37 @@
-use std::io::{Write,Result};
+use std::io::{Read,Write,Result};
+use std::fs::File;
 use std::str;
+use thoughtfuck::vm::*;
+use thoughtfuck::program::*;
+use thoughtfuck::parse::parse;
 
 
-pub struct FauxStdout {
+fn read_file(filename: &str) -> String {
+    let mut file = File::open(filename).unwrap();
+    let mut source = String::new();
+    file.read_to_string(&mut source).unwrap();
+    source
+}
+
+
+pub fn test_output(input_filename: &str, expected_answer_filename: &str) {
+    let source = read_file(input_filename);
+
+    let commands = parse(&source);
+
+    let mut program = Program::new();
+    program.append(&commands);
+
+    let mut stdout = FauxStdout::new();
+    {
+        let mut vm = VM::new(&mut stdout);
+        program.execute(&mut vm);
+    }
+    assert_eq!(stdout.buffer, read_file(expected_answer_filename));
+}
+
+
+struct FauxStdout {
     pub buffer: String
 }
 
--- tests/integration.rs
+++ tests/integration.rs
@@ -2,40 +2,8 @@ extern crate thoughtfuck;
 
 mod common;
 
-use std::fs::File;
-use std::io::Read;
-
-use common::FauxStdout;
-use thoughtfuck::vm::*;
-use thoughtfuck::program::*;
-use thoughtfuck::parse::parse;
-
-fn read_file(filename: &str) -> String {
-    let mut file = File::open(filename).unwrap();
-    let mut source = String::new();
-    file.read_to_string(&mut source).unwrap();
-    source
-}
-
-
-fn test_output(input_filename: &str, expected_answer_filename: &str) {
-    let source = read_file(input_filename);
-
-    let commands = parse(&source);
-
-    let mut program = Program::new();
-    program.append(&commands);
-
-    let mut stdout = FauxStdout::new();
-    {
-        let mut vm = VM::new(&mut stdout);
-        program.execute(&mut vm);
-    }
-    assert_eq!(stdout.buffer, read_file(expected_answer_filename));
-}
-
 
 #[test]
 fn hello_world () {
-    test_output("./tests/hello.b", "./tests/hello.txt");
+    common::test_output("./tests/hello.b", "./tests/hello.txt");
 }
