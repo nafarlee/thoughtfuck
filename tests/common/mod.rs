76d80d0014f5869b084b414b38f4567147ff14b3
Nicholas Farley <nicholasafarley@gmail.com>
2017-09-29 20:37:35 -0500

    Create a more correct implementation of FauxStdout.write

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -9,8 +9,13 @@ pub struct FauxStdout {
 
 impl Write for FauxStdout {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        self.buffer += str::from_utf8(buf).expect("u8 slice provided to FauxStdout.write() was not valid UTF-8");
-        Ok(0)
+        match str::from_utf8(buf) {
+            Err(e) => panic!(e),
+            Ok(s) => {
+                self.buffer += s;
+                return Ok(s.len());
+            }
+        }
     }
 
     fn flush(&mut self) -> Result<()> {
